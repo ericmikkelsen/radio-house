261dcb6f90f3d3b8896ef45edd3d15bb8388f705
ericmikkelsen <eric@ericmikkelsen.com>
2018-07-28 15:45:07 -0500

    feat: adds index page

---
+++ pages/index.js
@@ -0,0 +1,69 @@
+import Controls from '../components/Controls'
+import Layout from '../components/Layout' 
+import TrackList from '../components/TrackList' 
+import {Component} from 'react'
+
+export default class Index extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      browser: false,
+      files: [],
+      track: 0,
+    };
+  }
+
+  componentDidMount (){
+    // this sets the 
+    this.setState({ browser: true });
+    this.URL = window.URL || window.webkitURL
+    this.audio = document.createElement("audio");
+    this.play = () => {
+      this.audio.play()
+    }
+    this.pause = () => {
+      this.audio.pause()
+    }
+    this.nextTrack = () => {
+      this.playTrack( (this.state.track + 1) )
+    }
+
+    this.lastTrack = () => {
+      this.playTrack( ( this.state.track + 1 ) )
+    }
+  }
+  
+  playTrack = ( index ) => {
+    if( this.state.browser ){
+      const file = this.state.files[index]
+      this.setState({track: index})
+      this.audio.src = this.URL.createObjectURL(file)
+      this.play()
+    }
+  }
+
+  loadFiles = ( event ) => {
+    let newFiles = Object.values( event.target.files )
+    let oldFiles = this.state.files
+    let files = oldFiles.concat(newFiles)
+    this.setState( { files:  files } )
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Controls 
+          loadFiles={this.loadFiles} 
+          play={this.play}
+          pause={this.pause}
+        />
+        { this.state.files.length > 0 && 
+          <TrackList 
+            files={ this.state.files } 
+            playTrack={ this.playTrack } 
+          />
+        }
+      </Layout>
+    )
+  }
+}
