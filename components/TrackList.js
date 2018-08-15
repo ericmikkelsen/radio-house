7249f0c033915b960e3325165f566f52662a7b95
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-14 17:25:23 -0700

    feat: adds currentlyPlaying to TrackList

--- components/TrackList.js
+++ components/TrackList.js
@@ -2,11 +2,22 @@ import Track from './Track'
 import toCamelCase from '../modules/toCamelCase'
 
 export default (props) =>
-    <section>
-        <h2>Track List</h2>
+    <section className="TrackList__wrapper">
         <ul className="TrackList">
             { props.files.map( (file, index) => 
                 <li key={ toCamelCase('track ' + file.name) } >
+                    {props.currentTrack === index && 
+                    <p className="TrackList__currentlyPlaying">
+                        <svg 
+                            aria-hidden="true"
+                            className="TrackList__icon" 
+                            fill="currentColor"
+                            width="8" height="8" viewBox="0 0 8 8" 
+                        >
+                            <use xlinkHref='#play-icon'></use>
+                        </svg> 
+                        Currently Playing
+                    </p>}
                     <Track 
                         file={file} 
                         index={index}
