4f48b99844c8b7e753d0e3a365c968fe1ca7dff3
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-14 17:24:02 -0700

    feat: Makes Controls use Icon component

--- components/Controls.js
+++ components/Controls.js
@@ -1,37 +1,74 @@
 import Button from './Button'
 import Input from './Input'
+import Icon from './Icon'
 import toCamelCase from '../modules/toCamelCase'
 import toKebabCase from '../modules/toKebabCase'
 
 export default (props) =>
-  <menu>
-
-    <Button
-        id={'play'}
-        onClick={props.play}
-        text='Play'
-    />
-    <Button
-        id={'pause'}
-        onClick={props.pause}
-        text='Pause'
-    />
-    <Button
-        id={'next'}
-        onClick={props.nextTrack}
-        text='Next'
-    />
-    <Button
-        id={'previous'}
-        onClick={props.previousTrack}
-        text='Previous'
-    />
+  <menu className="Controls">
     <Input
+        className={'Control'}
         id="file-upload" 
-        label="Add directory or song files"
+        label={
+            <React.Fragment>
+                <span className="Control__label">Add</span>
+                <Icon 
+                    ariaHidden='true'
+                    className='Control__icon' 
+                    icon='add' 
+                />
+            </React.Fragment>
+        }
         multiple={true}
         onChange={props.loadFiles}
         type="file"
-        webkitdirectory="true"
     />
+    <Button
+        className={'Control'}
+        id={'play'}
+        onClick={props.play}
+    >
+        <span className="Control__label">Play</span>
+        <Icon 
+            ariaHidden='true'
+            className='Control__icon' 
+            icon='play' 
+        />
+    </Button>
+    <Button
+        className={'Control'}
+        id={'pause'}
+        onClick={props.pause}
+    >
+        <span className="Control__label">Pause</span>
+        <Icon 
+            ariaHidden='true'
+            className='Control__icon' 
+            icon='pause' 
+        />
+    </Button>
+    <Button
+        className={'Control'}
+        id={'previous'}
+        onClick={props.previousTrack}
+    >
+        <span className="Control__label">Previous</span>
+        <Icon 
+            ariaHidden='true'
+            className='Control__icon' 
+            icon='previous' 
+        />
+    </Button>
+    <Button
+        className={'Control'}
+        id={'next'}
+        onClick={props.nextTrack}
+    >
+        <span className="Control__label">Next</span>
+        <Icon 
+            ariaHidden='true'
+            className='Control__icon' 
+            icon='next' 
+        />
+    </Button>
   </menu>
