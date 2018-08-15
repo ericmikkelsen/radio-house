745674b56101c0378f836db0042fa9f1bb3b2984
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-14 17:28:46 -0700

    feat: adds id3 lib to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
+import {Component} from 'react'
 import Controls from '../components/Controls'
 import Layout from '../components/Layout' 
 import TrackList from '../components/TrackList' 
-import {Component} from 'react'
+import universalParse from 'id3-parser/lib/universal'
 
 export default class Index extends Component {
   constructor(props) {
@@ -17,27 +18,49 @@ export default class Index extends Component {
     // this sets the 
     this.setState({ browser: true });
     this.URL = window.URL || window.webkitURL
-    this.audio = document.createElement('audio');
+    this.audio = document.createElement('audio')
 
     // songs
     this.play = () => { this.audio.play() }
     this.pause = () => { this.audio.pause() }
-    this.nextTrack = () => { this.playTrack( ( this.state.track + 1 ) ) }
-    this.previousTrack = () => { this.playTrack( ( this.state.track + 1 ) ) }
+    this.nextTrack = () => {this.playTrack((this.state.track + 1))}
+    this.previousTrack = () => {this.playTrack((this.state.track - 1))}
+    
+    this.audio.addEventListener( 'ended', this.nextTrack );
   }
-  playTrack = ( index ) => {
+  playTrack = ( index = undefined ) => {
     if( this.state.browser ){
-      const file = this.state.files[index]
       this.setState({track: index})
+      const file = this.state.files[index]
       this.audio.src = this.URL.createObjectURL(file)
       this.play()
     }
   }
+
+  addID3Tags = ( file ) => {
+    file.id3 = id3(file);
+    return file;
+  }
+
+  addID3TagToFile = ( file ) => {
+    return universalParse( file )
+    .then(tag => {
+        file.id3 = tag
+        return file
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
   loadFiles = ( event ) => {
-    let newFiles = Object.values( event.target.files )
-    let oldFiles = this.state.files
-    let files = oldFiles.concat(newFiles)
-    this.setState( { files:  files } )
+    let newFiles = Object.values( event.target.files ).map(this.addID3TagToFile);
+    Promise.all(newFiles).then( newFiles => {
+      console.log(newFiles);
+      let oldFiles = this.state.files
+      let files = oldFiles.concat(newFiles)
+      this.setState( { files:  files } )
+    })
   }
 
   render() {
@@ -45,15 +68,16 @@ export default class Index extends Component {
       <Layout>
         <Controls 
           loadFiles={this.loadFiles} 
-          play={this.play}
           pause={this.pause}
+          play={this.play}
           nextTrack={this.nextTrack}
           previousTrack={this.previousTrack}
         />
         { this.state.files.length > 0 && 
-          <TrackList 
-            files={ this.state.files } 
-            playTrack={ this.playTrack } 
+          <TrackList
+            currentTrack={this.state.track}
+            files={this.state.files}
+            playTrack={this.playTrack}
           />
         }
       </Layout>
