2449717f839d9f01118c3233d384274207e7913a
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-14 17:20:50 -0700

    feat: adds id3 tag info

--- components/Track.js
+++ components/Track.js
@@ -1,7 +1,13 @@
 import Button from './Button'
 
 export default (props) =>
-    <Button
-        text={props.file.name}
-        onClick={ () => props.playTrack(props.index) }
-    />
+    <React.Fragment>
+        <Button
+            className="Track"
+            text={props.file.id3.title}
+            onClick={ () => props.playTrack(props.index) }
+        />
+        <p className="Track__meta">
+            by {props.file.id3.artist} <br/>on {props.file.id3.album}
+        </p>
+    </React.Fragment>
