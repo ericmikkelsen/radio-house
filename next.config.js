17b431db6b197270ed44a7fa3cb091e615ed0192
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-14 20:26:10 -0700

    feat: preact output

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
-module.exports = {
-  exportPathMap: function () {
+// next.config.js
+const withPreact = require('@zeit/next-preact')
+
+module.exports = withPreact( {
+  exportPathMap: () => {
     return {
       '/': { page: '/' }
     }
+  },
+  webpack: (config, options) => {
+    return config
   }
-}
+} )
