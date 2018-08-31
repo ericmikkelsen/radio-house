c84c41e9ddc1a0d675f19e22f9d99d096c0ee83e
ericmikkelsen <eric@ericmikkelsen.com>
2018-08-30 22:38:14 -0700

    feat: adds 600 font-weight to currently playing and removes play icon

--- components/TrackList.js
+++ components/TrackList.js
@@ -8,14 +8,6 @@ export default (props) =>
                 <li key={ toCamelCase('track ' + file.name) } >
                     {props.currentTrack === index && 
                     <p className="TrackList__currentlyPlaying">
-                        <svg 
-                            aria-hidden="true"
-                            className="TrackList__icon" 
-                            fill="currentColor"
-                            width="8" height="8" viewBox="0 0 8 8" 
-                        >
-                            <use xlinkHref='#play-icon'></use>
-                        </svg> 
                         Currently Playing
                     </p>}
                     <Track 
